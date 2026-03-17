63c37bc164688861519df4b94e61b60f98a6bb63
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-03-17 20:34:38 -0300

    file them add

---
+++ lib/thema/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color firstColor = Color(0xff2f6690);
+  static const Color secondColor = Color(0xff3a7ca5);
+  static const Color thirdColor = Color(0xffd9dcd6);
+  static const Color fourthColor = Color(0xff16425b);
+  static const Color fifthColor = Color(0xff81c3d7);
+  static const Color whiteColor = Color(0xffffffff);
+}
