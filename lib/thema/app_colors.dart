6d15052ce2a832ffc664bc3d2bea05b8122bf20b
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-03-31 22:35:36 -0300

    novas cores

--- lib/thema/app_colors.dart
+++ lib/thema/app_colors.dart
@@ -8,4 +8,9 @@ class AppColors {
   static const Color fifthColor = Color(0xff81c3d7);
   static const Color whiteColor = Color(0xffffffff);
   static const Color blackColor = Color(0x0F383838);
+  static const Color primary = Color(0xFFD62828); // vermelho
+  static const Color secondary = Color(0xFFFFC300); // amarelo
+  static const Color background = Color(0xFF0B090A); // fundo
+  static const Color card = Color(0xFF161A1D); // cards
+  static const Color accent = Color(0xFFFF6D00); // glow
 }
